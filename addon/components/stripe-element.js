5810be1bd55bd28c7f82032e2ff8379acf337443
Josemar Luedke <230476+josemarluedke@users.noreply.github.com>
2019-08-02 06:18:52 -0700

    Remove jQuery usage (#56)
    * Remove jQuery usage from stripe-element component
    * Add @ember/optional-features to disable jQuery in dummy app

--- addon/components/stripe-element.js
+++ addon/components/stripe-element.js
@@ -48,9 +48,9 @@ export default Component.extend({
     // Fetch autofocus, set by user
     let autofocus = get(this, 'autofocus');
     let stripeElement = get(this, 'stripeElement');
-    let $iframe = this.$('iframe')[0];
-    if (autofocus && $iframe) {
-      $iframe.onload = () => {
+    let iframe = this.element.querySelector('iframe')
+    if (autofocus && iframe) {
+      iframe.onload = () => {
         stripeElement.focus();
       };
     }
