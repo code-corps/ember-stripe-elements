4dc96f2c98b26b9744371767bbfcc0323bd1be19
Pablo Brasero <pablo@pablobm.com>
2019-08-02 13:58:46 +0100

    Mock also needed the functions for sinon to work

--- addon/utils/stripe-mock.js
+++ addon/utils/stripe-mock.js
@@ -13,9 +13,18 @@ StripeMock.prototype.elements = function() {
     }
   };
 }
-StripeMock.prototype.createToken = function() {}
-StripeMock.prototype.createSource = function() {}
-StripeMock.prototype.retrieveSource = function() {}
-StripeMock.prototype.paymentRequest = function() {}
+StripeMock.prototype.createToken = function() {};
+StripeMock.prototype.createSource = function() {};
+StripeMock.prototype.createPaymentMethod = function() {};
+StripeMock.prototype.retrieveSource = function() {};
+StripeMock.prototype.paymentRequest = function() {};
+StripeMock.prototype.redirectToCheckout = function() {};
+StripeMock.prototype.retrievePaymentIntent = function() {};
+StripeMock.prototype.handleCardPayment = function() {};
+StripeMock.prototype.handleCardAction = function() {};
+StripeMock.prototype.confirmPaymentIntent = function() {};
+StripeMock.prototype.handleCardSetup = function() {};
+StripeMock.prototype.retrieveSetupIntent = function() {};
+StripeMock.prototype.confirmSetupIntent = function() {};
 
 export default StripeMock;
